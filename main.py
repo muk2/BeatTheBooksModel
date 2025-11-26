eb43b688809c11afd7421c01177295610a5b39f2
muk <themu321@gmail.com>
2025-11-25 19:15:00 -0500

    scrape route, implement service/repository

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ app = FastAPI()
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
+
+
+
+@app.get("/scrape")
+async def scrape_data():
+
+    
+
+
+    return {"message": "Scraping data... (functionality not yet implemented)"}
